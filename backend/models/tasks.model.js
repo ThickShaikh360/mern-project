51ef2e7b278592b470163f72ccb2f33538800250
Azan <azan16shaikh@gmail.com>
2025-04-01 13:46:48 +0530

    modified the task schema/model to implement three task states

--- backend/models/tasks.model.js
+++ backend/models/tasks.model.js
@@ -2,7 +2,12 @@ import mongoose from "mongoose";
 
 const taskschema = new mongoose.Schema({
     name:{type: String,required:true},
-    status: {type:Boolean},
+    status: { 
+        type: String, 
+        enum: ["ongoing", "willdo", "todo"], // Allowed values
+        default: "willdo", // Default value
+        required: true 
+    },
     timestamps:true
 });
 
