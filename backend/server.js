1b1056f1ada73aedb5c03c64ec9d27634e106a91
Azan <azan16shaikh@gmail.com>
2025-04-04 05:09:47 +0530

    added application endpoints for CRUD ops

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,9 @@ import Task from './models/tasks.model.js';
 dotenv.config();
 const app = express();
 
-
 app.use(express.json()); //allows you to use json in req.body 
 
-app.post("/api/tasks",async(req,res)=>{
-
+app.post("/api/task",async(req,res)=>{
     const task = req.body;
 
     if(!task){
@@ -30,7 +28,45 @@ app.post("/api/tasks",async(req,res)=>{
     }
 });
 
+
+app.delete('/api/delete/:id',async (req,res)=>{
+    const {id} = req.params;
+    try {
+        await Task.findByIdAndDelete(id);
+        res.status(200).json({success:true, message:"task deleted"});
+    } catch (error) {
+        res.status(404).json({success:false,message:"unable to delete the task or task not found"});
+    }
+});
+
+
+app.get('/api/tasks',async (req,res)=>{
+    try{
+        const tasks = await Task.find({});
+        res.status(200).json({success:true,message:"tasks retrieved", data:tasks })
+    }
+    catch(error){
+        console.log(error.message)
+        res.status(500).json({success:false ,message:"unable to fetch tasks"})
+    }
+});
+
+
+app.put("/api/task/:id",async (req,res)=>{
+    const {id} = req.params;
+
+    const tasktoupdate = req.body;    
+
+    try {
+        const updatedtask = await Task.findByIdAndUpdate(id,tasktoupdate,{new:true})
+        res.status(200).json({success:true,message:"task updated",data:updatedtask})
+    } catch (error) {
+        console.log(error.message)
+    }
+})
+
 app.listen(3000,(req,res)=>{
+
     connectDB();
     console.log('Server running on port 3000');
 });
